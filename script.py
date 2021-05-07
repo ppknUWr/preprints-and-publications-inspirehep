b19ac323fac862b023d0d56951ce454ce41d895d
MikolajMagaj <322990@uwr.edu.pl>
2021-05-08 00:14:22 +0200

    alfa 1.0

--- script.py
+++ script.py
@@ -15,66 +15,73 @@ parser.add_argument("-s", "--size", dest="s", type=int,
 parser.add_argument("-y", "--year", dest="y", type=str,
                     help="Enter the year. E.g --2019 means all years to 2019, 2019--2019 means this one year, 2019-- means 2019 and upper")
 
-args = parser.parse_args()
-file = args.o
-name = 'a%20' + args.a.replace(' ', '%20')
+args = parser.parse_args()                  # rozbicie
+file = args.o                               # nazwa pliku do jakiego trafi
+name = 'a%20' + args.a.replace(' ', '%20')  # dest=a, zamienia spacje na odpowiednie znaki
 if args.y == None:
     date = ""
 else:
     date = '&earliest_date=' + args.y
 
+# pobranie by poznac dlugosc
 size = args.s
 if size == None:
     inspirehep_profile = 'https://inspirehep.net/api/literature?q=' + name + date + '&format=json'
     data = json.loads(urllib.request.urlopen(inspirehep_profile).read())
     size = data['hits']['total']
-inspirehep_profile = 'https://inspirehep.net/api/literature?q=' + name + date + "&sort=mostrecent&size=" + str(size) + '&format=json'
+
+# pobranie wlasciwe
+inspirehep_profile = 'https://inspirehep.net/api/literature?q=' + name + "%20and%20d%202019&size=" + str(size) + '&format=json '
 data = json.loads(urllib.request.urlopen(inspirehep_profile).read())
+
+# tablice na dane
 titles = []
 links_to_articles = []
-authors = []
-links_to_authors = []
-print(len(data['hits']['hits']))
+authors = []                            # tablica tablic autorow
+links_to_authors = []                   # tablica tablic linkow autorow
+
 for hit in data['hits']['hits']:
     titles.append(hit['metadata']['titles'][0]['title'])
     links_to_articles.append('https://inspirehep.net/literature/' + str(hit['metadata']['control_number']))
     authors_hit = []
     links_to_authors_hit = []
-    for author in hit['metadata']['authors']:
+    for author in hit['metadata']['authors']:   # autorow wiecej niz 1 dla publikacji
         authors_hit.append(author['first_name'] + ' ' + author['last_name'])
         if 'record' not in author:
-            links_to_authors_hit.append("-")
+            links_to_authors_hit.append("-")    # gdy nie ma linku dla autora
         else:
             links_to_authors_hit.append(author['record']['$ref'])
     authors.append(authors_hit)
     links_to_authors.append(links_to_authors_hit)
-print(inspirehep_profile)
+
 container = ET.Element('div')
 for i in range(len(titles)):
     div = ET.Element('div')
     p_title = ET.Element('p')
-    div.append(p_title)
     b_title = ET.Element('b')
-    p_title.append(b_title)
     a_title = ET.Element('a', attrib={'href': links_to_articles[i]})
-    a_title.text = titles[i]
-    b_title.append(a_title)
     p_authors = ET.Element('p')
+
+    div.append(p_title)         # dodaj tytul publikacji
+    p_title.append(b_title)     # dodaj pogrubienie
+    a_title.text = titles[i]    # dodaj link do publikaji
+    b_title.append(a_title)     # dodaj link do do znacznika b
+
     for j in range(len(links_to_authors[i])):
         if j == 0:
             p_authors.text = "By "
-        if(links_to_authors[i][j] != "-"):
-            a_authors = ET.Element('a', attrib={'href': "".join(links_to_authors[i][j].split("/api"))}) 
+        if links_to_authors[i][j] != "-":
+            a_authors = ET.Element('a', attrib={'href': "".join(links_to_authors[i][j].split("/api"))})
             if j == len(links_to_authors[i]) - 1:
-                # zrobic zeby kropki i przeciniki byly w p a nie a 
+                # zrobic zeby kropki i przeciniki byly w p a nie a
                 a_authors.text = authors[i][j] + "."
             else:
-                a_authors.text = authors[i][j]+", "
+                a_authors.text = authors[i][j] + ", "
             p_authors.append(a_authors)
         else:
             if j == len(links_to_authors[i]) - 1:
                 span_out_authors = ET.Element('span')
-                span_out_authors.text = authors[i][j] +", "
+                span_out_authors.text = authors[i][j] + ", "
                 p_authors.append(span_out_authors)
     div.append(p_authors)
     container.append(div)
